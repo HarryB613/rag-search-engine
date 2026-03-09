2fe66a651ebb0ac33dcbb535efe162858a7c6648
HarryB613 <HarryB613@gmail.com>
2026-03-09 14:06:25 -0700

    punctuation

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,11 +1,14 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+import string
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
+    stripped_query = query.lower().translate(str.maketrans("", "", string.punctuation))
     for movie in movies:
-        if query.lower() in movie["title"].lower():
+        movie_title = movie["title"].lower().translate(str.maketrans("", "", string.punctuation))
+        if stripped_query in movie_title:
             results.append(movie)
             if len(results) >= limit:
                 break
