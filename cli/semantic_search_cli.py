f4e15b3bb945e413096e51ab61fcbafba1b0cbb0
HarryB613 <HarryB613@gmail.com>
2026-04-12 06:43:56 -0700

    chunking

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -3,6 +3,7 @@
 import argparse
 
 from lib.semantic_search import (
+    chunk_text,
     embed_query_text,
     embed_text,
     semantic_search,
@@ -38,6 +39,14 @@ def main():
         "--limit", type=int, default=5, help="Number of results to return"
     )
 
+    chunk_parser = subparsers.add_parser(
+        "chunk", help="Split text into fixed-size chunks"
+    )
+    chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    chunk_parser.add_argument(
+        "--chunk-size", type=int, default=200, help="Size of each chunk in words"
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -51,6 +60,8 @@ def main():
             embed_query_text(args.query)
         case "search":
             semantic_search(args.query, args.limit)
+        case "chunk":
+            chunk_text(args.text, args.chunk_size)
         case _:
             parser.print_help()
 
